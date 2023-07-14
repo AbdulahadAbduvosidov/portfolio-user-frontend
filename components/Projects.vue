435fa157a885e9361f3cf80a41ec8549b63f45ab
AbdulahadAbduvosidov <abduaxadabduvosidov@gmail.com>
2023-07-14 20:41:45 +0500

    xabar

--- components/Projects.vue
+++ components/Projects.vue
@@ -35,7 +35,7 @@
                         <h1 class="text-[28px] mt-[30px] text-[#7562E0]">Test system</h1>
                         <p class="text-[20px] mt-[10px]">in this site you can get detailed information about testing students.</p>
                         <div class="flex justify-center mt-[40px] pb-6">
-                            <NuxtLink to="https://github.com/Test-system-edu/frontend">
+                            <NuxtLink to="https://test-project-pi-nine.vercel.app/">
                                 <button class="live_btn bg-[#7562E0] w-[200px] h-[50px] rounded-[8px] text-[20px]">View Live</button>
                             </NuxtLink>
                         </div>
